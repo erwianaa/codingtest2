43f095f3218ad581ea6b8efe0f97aa51a934d391
erwianaa <erwianaa02@gmail.com>
2024-09-09 19:21:33 +0700

    init 5 menit

---
+++ index.js
@@ -0,0 +1,13 @@
+// input			// output
+// "hello"   			// { h: 1, e: 1, l: 2, o: 1 }
+// "aabbbcc"			// { a: 2, b: 3, c: 2 }
+
+var soal1 = "hello"
+var soal2 = "aabbbcc"
+
+function jawabsoal(soal) {
+     return hasil;
+ }
+ 
+ console.log(soal1, jawabsoal(soal1));
+ console.log(soal2, jawabsoal(soal2));
